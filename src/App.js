1cc45d07ea84e387d5ba06bea5993d1fec9679cc
Jamie Hale <jamiesonhale@gmail.com>
2018-08-02 19:44:20 -0400

    Extract functional components

--- src/App.js
+++ src/App.js
@@ -11,6 +11,48 @@ import './App.css';
 
 let context;
 
+const Result = props => (
+  <div>
+    <h2>Result</h2>
+    <p>
+      { props.result }
+    </p>
+  </div>
+);
+
+const Output = props => {
+  const outputLines = props.output.map((l, i) => <li key={ i }>{ l }</li>);
+
+  return (
+    <div>
+      <h2>Output</h2>
+      <ul>
+        { outputLines }
+      </ul>
+    </div>
+  );
+};
+
+const ClearButton = props => (
+  <button onClick={ props.onClear }>Clear</button>
+);
+
+const Code = props => (
+  <div>
+    <form onSubmit={ props.onSubmit }>
+      <label>
+        Script:
+        <Editor script={ props.script } onChange={ props.onChange } />
+      </label>
+      <input type="submit" value="Run" />
+    </form>
+  </div>
+);
+
+const Editor = props => (
+  <textarea name="script" value={ props.script } onChange={ props.onChange }></textarea>
+);
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -80,20 +122,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p>
-          Result: { this.state.result }
-        </p>
-        <p>
-          Output: { this.state.output.join("\n") }
-        </p>
-        <button onClick={ this.onClear }>Clear</button>
-        <form onSubmit={ this.handleSubmit }>
-          <label>
-            Script:
-            <textarea name="script" value={ this.state.script } onChange={ this.handleChange }></textarea>
-          </label>
-          <input type="submit" value="Run" />
-        </form>
+        <Result result={ this.state.result } />
+        <Output output={ this.state.output } />
+        <ClearButton onClear={ this.onClear } />
+        <Code script={ this.state.script } onChange={ this.handleChange } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
